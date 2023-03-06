b8c779c415d25b2b6c80bfa53666dc5472bb8878
Jubayer Abdullah Joy <jubayerjoy98@gmail.com>
2023-03-07 00:00:02 +0600

    feat: dotenv and eslint

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,21 +1,17 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
+import * as dotenv from 'dotenv';
 import express from 'express';
-import * as path from 'path';
 
+dotenv.config();
 const app = express();
-
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to server!' });
 });
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3000;
+
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
+
 server.on('error', console.error);
